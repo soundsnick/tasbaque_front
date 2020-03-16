ac3812baeee840f68daef7485777ced8179e1a5b
soundsnick <soundsnick@gmail.com>
2020-03-16 09:25:02 +0600

    asd

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="warning">
+      Дорогие, жюри. В своем проекте мы использовали NLP ML, разработка заняла много времени, и бэк у нас практический не готов и фронт работает с текстом-рыбой. Но мы успешно протестировали модели и сохранили эмбеддинги в CSV, надеемся до вечера доделать проект и показать рабочий ресурс.
+    </div>
     <div>
       <h1 class="title">Новостная лента</h1>
       <div class="posts">
@@ -8,7 +11,20 @@
     </div>
   </div>
 </template>
-
+<style scoped>
+  .warning{
+    margin: 20px 0;
+    padding: 20px;
+    background: orange;
+    color: #fff;
+    line-height: 1.5;
+    font-size: 12px;
+    text-transform: uppercase;
+    font-weight: 600;
+    border-radius: 10px;
+    box-shadow: 0 2px 6px rgba(0,0,0,0.05);
+  }
+</style>
 <script>
 import Logo from '~/components/Logo.vue'
 import Post from '~/components/Post.vue'
